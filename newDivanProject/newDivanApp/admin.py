a12e845e4db374f6132e4f30c1ffe7837963e2d7
Лим Анастасия Александровна <a.lim@spbtechnologies.ru>
2024-05-10 19:58:20 +0300

    изменения в models.py, admin.py

--- newDivanProject/newDivanApp/admin.py
+++ newDivanProject/newDivanApp/admin.py
@@ -3,9 +3,9 @@ from .models import *
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['position', 'department', 'status', 'employment_date', 'avatar', 'first_name', 'last_name',]
+    list_display = ['position', 'department', 'status', 'employment_date', 'avatar', 'first_name', 'last_name']
     list_filter = ['department', 'status']
-    search_fields = ['position', 'department', 'residence_address', 'passport_number']
+    search_fields = ['first_name', 'last_name', 'residence_address', 'passport_number']
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
@@ -13,15 +13,13 @@ class DepartmentAdmin(admin.ModelAdmin):
 
 @admin.register(JobTitle)
 class JobTitleAdmin(admin.ModelAdmin):
-    list_display = ['name', 'descr', 'access_lvl']
-
+    list_display = ['name', 'descr']
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'middle_name', 'contact_number', 'address', 'comments']
     list_filter = ['address']
     search_fields = ['first_name', 'last_name', 'contact_number', 'address']
-
 
 @admin.register(Firm)
 class FirmAdmin(admin.ModelAdmin):
@@ -30,51 +28,44 @@ class FirmAdmin(admin.ModelAdmin):
     list_filter = ['legal_address', 'actual_address', 'INN']
     search_fields = ['short_name', 'full_name', 'INN', 'OGRN']
 
-
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
-    list_display = ['client', 'firm', 'create_date', 'completion_date', 'duration', 'total_value', 'total_work_cost',
-                    'prepayment_share', 'prepayment_value', 'prepayment_date', 'is_prepayment_paid', 'payment_date',
-                    'is_balance_paid', 'comments']
-    list_filter = ['create_date', 'completion_date', 'is_prepayment_paid', 'is_balance_paid']
-    search_fields = ['client_last_name', 'firm_short_name', 'total_value']
-
+    list_display = ['num', 'client', 'firm', 'create_date', 'completion_date', 'duration', 'total_value', 
+                    'total_work_cost', 'payment_type', 'prepayment_share', 'prepayment_value', 'prepayment_date', 
+                    'is_prepayment_paid', 'postpayment_value', 'postpayment_date', 'is_postpayment_paid', 'comments']
+    list_filter = ['create_date', 'completion_date', 'is_prepayment_paid', 'is_postpayment_paid']
+    search_fields = ['client__last_name', 'firm__short_name', 'total_value']
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['number', 'contract', 'manager', 'status', 'executor1', 'executor2', 'executor3', 'comments']
-    list_filter = ['status', 'contract', 'manager', 'executor1', 'executor2', 'executor3']
-    search_fields = ['number', 'manager_first_name', 'manager_last_name']
-
+    list_display = ['number', 'contract', 'manager', 'source', 'status', 'executor1', 'executor2', 'executor3', 'comments']
+    list_filter = ['status', 'source', 'manager', 'executor1', 'executor2', 'executor3']
+    search_fields = ['number', 'manager__first_name', 'manager__last_name']
 
 @admin.register(TechnicalSpecification)
 class TechnicalSpecificationAdmin(admin.ModelAdmin):
     list_display = ['order', 'items_qty', 'short_descr', 'full_descr', 'work_type1', 'work_type2', 'furniture_type1',
-                    'furniture_type2', 'comments', 'photo1', 'photo2', 'photo3', 'photo4']
+                    'furniture_type2', 'item_type', 'comments', 'photo1', 'photo2', 'photo3', 'photo4']
     list_filter = ['work_type1', 'work_type2', 'furniture_type1', 'furniture_type2']
-    search_fields = ['short_descr', 'full_descr']
-
+    search_fields = ['short_descr', 'full_descr', 'item_type']
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ['order', 'employee', 'status', 'activity_descr', 'date_start', 'date_review', 'date_end',
                     'total_work_cost', 'is_paid', 'payment_date', 'photo1', 'photo2', 'photo3', 'photo4']
     list_filter = ['status', 'date_start', 'date_end', 'is_paid']
-    search_fields = ['order_number', 'employee_last_name']
-
+    search_fields = ['order__number', 'employee__last_name']
 
 @admin.register(Material)
 class MaterialAdmin(admin.ModelAdmin):
-    list_display = ['order', 'material_name', 'material_in_stock', 'is_ordered', 'material_cost',
-                    'material_payment_status', 'material_payment_date', 'needs_fitting', 'fitting_name',
-                    'fitting_in_stock', 'comments']
-    list_filter = ['material_in_stock', 'is_ordered', 'material_payment_status']
-    search_fields = ['material_name', 'order_number']
-
+    list_display = ['order', 'name', 'in_stock', 'order_status', 'cost', 'order_date', 'payment_status', 'payment_date',
+                    'fitting_name', 'fitting_in_stock', 'comments']
+    list_filter = ['in_stock', 'order_status', 'payment_status']
+    search_fields = ['name', 'order__number']
 
 @admin.register(PickupDelivery)
 class PickupDeliveryAdmin(admin.ModelAdmin):
-    list_display = ['order', 'is_picked', 'pickup_type', 'is_delivered', 'delivery_type', 'estimated_pickup_date',
-                    'actual_pickup_date', 'estimated_delivery_date', 'actual_delivery_date', 'delivery_guy', 'comments']
+    list_display = ['order', 'is_picked', 'pickup_date', 'pickup_time', 'pickup_type', 'pickup_guy', 'is_delivered',
+                    'delivery_date', 'delivery_time', 'delivery_type', 'delivery_guy', 'pickup_comments', 'delivery_comments']
     list_filter = ['is_picked', 'is_delivered', 'pickup_type', 'delivery_type']
-    search_fields = ['order_number', 'pickup_type', 'delivery_type']
+    search_fields = ['order__number', 'pickup_type', 'delivery_type']
